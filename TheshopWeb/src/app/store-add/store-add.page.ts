c016b9eeb0d9f40ddb4e70064db857732419bf57
stampnoon <thnong_stamp@hotmail.com>
2020-02-20 11:37:49 +0700

    Reversion

--- TheshopWeb/src/app/store-add/store-add.page.ts
+++ TheshopWeb/src/app/store-add/store-add.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core/';
 import { Router } from '@angular/router';
 import { CallapiService } from '../service/callapi.service';
 import { product } from '../models/product';
